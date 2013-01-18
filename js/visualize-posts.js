8b645fdf722be151e5fc9d64da52bd6470788f47
Felipe Lavín Z <felipe@yukei.net>
2013-01-18 00:54:02 -0300

    working prototype

--- js/visualize-posts.js
+++ js/visualize-posts.js
@@ -1,7 +1,35 @@
-(function($, data){
+(function($){
 	google.load('visualization', '1.0', {'packages':['corechart']});
 	google.setOnLoadCallback( visualizePosts );
 	function visualizePosts(){
-
+		$.get( ajaxurl, {
+			action: 'get_visualize_post_data'
+		}, function(json){
+			var data = google.visualization.arrayToDataTable( json );
+			var options = {
+				height: 120,
+				axisTitlesPosition: 'none',
+				backgroundColor: {
+					fill: 'transparent'
+				},
+				hAxis: {
+					textPosition: 'none'
+				},
+				vAxis: {
+					gridlines: { color: '#ececec' },
+					textStyle: { color: '#999', fontSize: 10 },
+					baselineColor: '#ececec'
+				},
+				legend: {
+					position: 'none'
+				},
+				theme: 'maximized',
+				lineWidth: 3,
+				pointSize: 6,
+				colors: [ '#3399CC' ]
+			},
+			chart = new google.visualization.AreaChart( document.getElementById('visualize-posts-canvas') );
+			chart.draw( data, options );
+		}, 'json');
 	}
-})(jQuery, visualizePostsData);
+})(jQuery);
--- visualize-post-actions.php
+++ visualize-post-actions.php
@@ -36,6 +36,7 @@ class VisualizePostActions{
 	private function setActions(){
 		add_action('wp_dashboard_setup', array($this, 'addDashboardWidget'));
 		add_action('admin_enqueue_scripts', array($this, 'enqueueScripts'));
+		add_action('wp_ajax_get_visualize_post_data', array($this, 'getDataAjax'));
 	}
 
 	public function enqueueScripts(){
@@ -43,34 +44,83 @@ class VisualizePostActions{
 		if ( $screen->base === 'dashboard' ) {
 			wp_enqueue_script( 'google-ajaxapi', '//www.google.com/jsapi', array(), static::plugin_version, true );
 			wp_enqueue_script( 'visualize-posts-script', plugins_url( '/js/visualize-posts.js', __FILE__ ), array('google-ajaxapi'), static::plugin_version, true );
-			wp_localize_script( 'visualize-posts-script', 'VisualizePosts', $this->getData() );
 		}
 	}
 
 	public function addDashboardWidget(){
-		wp_add_dashboard_widget( 'visualize-post-actions', __('Post actions', 'visualize_post_actions'), array($this, 'dashboardWidgetContent') );
+		wp_add_dashboard_widget( 'visualize-post-actions', __('Published posts', 'visualize_post_actions'), array($this, 'dashboardWidgetContent') );
 	}
 
+	public function getDataAjax(){
+		$data = $this->getData();
+		$out = array();
+		foreach ( $data as $key => $val ){
+			$out[] = array( $key, $val );
+		}
+		$out = array_reverse($out);
+		array_unshift($out, array('Date', 'Published posts'));
+		echo json_encode( $out );
+		exit;
+	}
 	private function getData(){
 		global $wpdb;
 		$out = new stdClass;
-		$published = $wpdb->get_results( $wpdb->prepare("SELECT CAST(post_date AS DATE) AS date, COUNT(ID) AS count FROM $wpdb->posts GROUP BY CAST(post_date AS DATE) ORDER BY post_date DESC LIMIT 30"), OBJECT_K );
+		$published = $wpdb->get_results( $wpdb->prepare("SELECT CAST(post_date AS DATE) AS date, COUNT(ID) AS count FROM $wpdb->posts WHERE post_status = 'publish' GROUP BY CAST(post_date AS DATE) ORDER BY post_date DESC LIMIT %d", 30), OBJECT_K );
+		// published entries by type
+		// SELECT
+		// 	CAST(post_date AS DATE) AS date,
+		// 	post_type,
+		// 	COUNT(post_type) AS post_type_count
+		// FROM $wpdb->posts
+		// WHERE
+		// 	post_status = 'publish'
+		// 	AND post_date > DATE_SUB(CURDATE(), INTERVAL 30 DAY)
+		// GROUP BY
+		// 	CAST(post_date AS DATE), post_type
+		// ORDER BY post_date DESC
 		$start_date = new DateTime( key($published) );
-		$pub_entries = array();
-		if ( $published ) {
-			$date_index  = clone $start_date;
-			for ( $i=0; $i<60; $i++ ){
+		$pub_entries = $this->prepareData( $published, 30, true, $start_date );
+		return $pub_entries;
+	}
+
+	/**
+	 * Prepare data points
+	 * @param array $data The data points that will be graphed, using keys as dates and count as values
+	 * @param int $limit The total amount of data entries to be processed
+	 * @param bool $chronological Whether to show days with no activity
+	 * @param DateTime $start_date The start date. If empty, the first key will be used
+	 * @return array
+	 */
+	private function prepareData( array $data, $limit = 30, $chronological = true, $start_date = null ){
+		$out = array();
+		if ( $chronological ) {
+			if ( is_null($start_date) ) {
+				// if no starting date it's given, use the key for the first data point
+				$start_date = new DateTime( key($data) );
+			}
+			if ( ! $start_date instanceof DateTime ){
+				throw new InvalidArgumentException( __('The $start_date parameter must be a DateTime object') );
+			}
+			$date_index = clone $start_date;
+			for ( $i = 0; $i < $limit; $i++ ){
 				$this_date = $date_index->format('Y-m-d');
-				$pub_entries[ $this_date ] = isset( $published[ $this_date ] ) ? (int)$published[ $this_date ]->count : 0;
+				$out[ $this_date ] = isset( $data[ $this_date ] ) ? (int)$data[ $this_date ]->count : 0;
 				$date_index = $date_index->sub( new DateInterval('P1D') );
 			}
+		} else {
+			foreach ( $data as $key => $val ) {
+				$this_date = new DateTime( $key );
+				$this_date = $this_date->format('Y-m-d');
+				$out[ $this_date ] = (int)$val->count;
+			}
 		}
-		$out->published = $pub_entries;
 		return $out;
 	}
 
 	public function dashboardWidgetContent(){
-		echo '<pre>', print_r($published, true) ,'</pre>';
+		echo '<div id="visualize-posts-canvas" style="height:120px">';
+			echo '<span class="description">Loading data&hellip;</span>';
+		echo '</div>';
 	}
 }
 // Instantiate the class object
